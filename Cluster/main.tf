d71c8fa7f7caf2435b19b9e90692dfd5cd8c6893
Shubham Gokhale <59763482+shubhamgokhale505@users.noreply.github.com>
2024-10-06 00:46:02 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_vpc" "shubham_vpc" {
@@ -14,7 +14,7 @@ resource "aws_subnet" "shubham_subnet" {
   count = 2
   vpc_id                  = aws_vpc.shubham_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.shubham_vpc.cidr_block, 8, count.index)
-  availability_zone       = element(["us-east-1a", "us-east-1b"], count.index)
+  availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
