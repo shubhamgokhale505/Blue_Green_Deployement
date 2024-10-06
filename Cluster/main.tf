6a04f54a17e4bd2a73437f5c8b816114cd1b130c
Shubham Gokhale <59763482+shubhamgokhale505@users.noreply.github.com>
2024-10-06 09:24:15 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -103,9 +103,9 @@ resource "aws_eks_node_group" "shubham" {
   subnet_ids      = aws_subnet.shubham_subnet[*].id
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   instance_types = ["t2.micro"]
